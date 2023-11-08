2c5298d56c7454d072473a914dca3746d5a1ff49
ashritav2004 <ashritav2004@gmail.com>
2023-11-08 15:24:25 -0600

    quicksort.cpp completed

---
+++ QuickSort/MPI/quicksort.cpp
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition(int* arr, int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quicksort(int* arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        if (pi - low < high - pi) {
+            quicksort(arr, low, pi - 1);
+            quicksort(arr, pi + 1, high);
+        } else {
+            quicksort(arr, pi + 1, high);
+            quicksort(arr, low, pi - 1);
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    if (world_size < 2) {
+        fprintf(stderr, "This program requires at least 2 processes.\n");
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+
+    if (world_rank == 0) {
+        int n = 10;
+        int h_arr[n] = {4, 1, 7, 3, 9, 8, 2, 5, 6, 0};
+
+        int* sub_arr = (int*)malloc(n * sizeof(int));
+        MPI_Scatter(h_arr, n / world_size, MPI_INT, sub_arr, n / world_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+        int local_low = 0;
+        int local_high = n / world_size - 1;
+        quicksort(sub_arr, local_low, local_high);
+
+        MPI_Gather(sub_arr, n / world_size, MPI_INT, h_arr, n / world_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+        free(sub_arr);
+
+        printf("Sorted array: ");
+        for (int i = 0; i < n; i++) {
+            printf("%d ", h_arr[i]);
+        }
+        printf("\n");
+    } else {
+        int n;
+        MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+        int* sub_arr = (int*)malloc(n / world_size * sizeof(int));
+        MPI_Scatter(NULL, 0, MPI_INT, sub_arr, n / world_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+        int local_low = 0;
+        int local_high = n / world_size - 1;
+        quicksort(sub_arr, local_low, local_high);
+
+        MPI_Gather(sub_arr, n / world_size, MPI_INT, NULL, 0, MPI_INT, 0, MPI_COMM_WORLD);
+
+        free(sub_arr);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
