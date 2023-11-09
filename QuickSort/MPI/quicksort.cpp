455c6f83c0233db84c1abbddc6177af3cf18543d
ashritav2004 <ashritav2004@gmail.com>
2023-11-08 22:43:36 -0600

    Added to quicksort.cpp

--- QuickSort/MPI/quicksort.cpp
+++ QuickSort/MPI/quicksort.cpp
@@ -10,6 +10,20 @@
 #include <caliper/cali-manager.h>
 #include <adiak.hpp>
 
+using namespace std;
+
+/* Define Caliper region names */
+const char* mainFunction = "main";
+const char* data_init = "data_init";
+const char* correctness_check = "correctness_check ";
+const char* comm = "comm";
+const char* comm_large = "comm_large";
+const char* comm_small = "comm _small";
+const char* comp = "comp";
+const char* comp_large = "comp_large";
+const char* comp_small = "comp_small";
+
+
 void swap(int* a, int* b) {
     int t = *a;
     *a = *b;
@@ -94,6 +108,25 @@ int main(int argc, char** argv) {
 
         free(sub_arr);
     }
+
+    adiak::init(NULL);
+    adiak::launchdate();    // launch date of the job
+    adiak::libraries();     // Libraries used
+    adiak::cmdline();       // Command line used to launch the job
+    adiak::clustername();   // Name of the cluster
+    adiak::value("Algorithm", "quicksort"); // The name of the algorithm you are using (e.g., "MergeSort", "BitonicSort")
+    adiak::value("ProgrammingModel", "MPI"); // e.g., "MPI", "CUDA", "MPIwithCUDA"
+    adiak::value("Datatype", "int"); // The datatype of input elements (e.g., double, int, float)
+    adiak::value("SizeOfDatatype", sizeof(int)); // sizeof(datatype) of input elements in bytes (e.g., 1, 2, 4)
+    adiak::value("InputSize", inputSize); // The number of elements in input dataset (1000)
+    adiak::value("InputType", inputType); // For sorting, this would be "Sorted", "ReverseSorted", "Random", "1%perturbed"
+    adiak::value("num_procs", numProcesses); // The number of processors (MPI ranks)
+    adiak::value("group_num", 16); // The number of your group (integer, e.g., 1, 10)
+    adiak::value("implementation_source", "Handwritten & Online"); // Where you got the source code of your algorithm; choices: ("Online", "AI", "Handwritten").
+
+    // Flush Caliper output before finalizing MPI
+   mgr.stop();
+   mgr.flush();
 
     MPI_Finalize();
     return 0;
